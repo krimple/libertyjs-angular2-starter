82de56e2a96e668f7df63a3daa5a8290d9037bcf
Michael Cebrian <michael.cebrian@gmail.com>
2016-09-28 23:01:38 -0400

    Cleanup main.ts
    Remove hammerjs and bootstrap includes from main.ts
    Replace bootstrap.css with bootstrap-flex.sass

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import './polyfills.ts';
-import 'hammerjs';   // for material design
-import '!style!css!sass!../node_modules/bootstrap/scss/bootstrap-flex.scss';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
